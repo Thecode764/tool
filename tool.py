f3b1090e9e1349624fb5bd6c9ec46937bddeaf75
Im Code <141347375+Thecode764@users.noreply.github.com>
2023-08-15 00:11:34 +0800

    Update tool.py

--- tool.py
+++ tool.py
@@ -73,4 +73,4 @@ if tool == "9":
     os.system("sudo apt-get install zsh")
     os.system("wget https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh")
 if tool == "10":
-    os.system("python2 katoolin.py")
+    os.system("sudo python2 katoolin.py")
